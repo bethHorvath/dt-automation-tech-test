4de99f89eac0f2bf3ad1d5b992374b2b99b4fbc3
CraigBallard <craig.ballard@sky.uk>
2023-08-23 16:44:30 +0100

    refactoring test

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,11 +1,21 @@
 const {test, expect } = require('@playwright/test');
 
+const skyUrl = 'https://sky.com';
+const cookiesIframe = '#notice';
+const acceptCta = "text=Accept all";
+const navList = '[data-test-id="primary-nav-list"]';
+const dealsUrl = 'https://www.sky.com/deals';
+
+
+
 test.describe("This feature will make sure that the shop page is navigable and usable", async() => {
     test('Navigate to Deals Page', async({page}) => {
-        await page.goto('https://sky.com', {waitUntil: "domcontentloaded"});
-        await page.frameLocator("#sp_message_iframe_474555").locator("text=Agree").click();
-        await page.locator('[data-test-id="primary-nav-list"] >> text="Deals"').click();
-        await expect(page.url()).toEqual('https://www.sky.com/deals', 'deal url does not match');
+        await page.goto(skyUrl, {waitUntil: "domcontentloaded"});
+        await page.frameLocator(cookiesIframe).locator(acceptCta).click;
+        const navListContainer = page.locator('[data-test-id="primary-nav-list"]');
+        // await page.locator('[data-test-id="primary-nav-list"] >> text="Deals"').click();
+        await navListContainer.getByText('Deals').click();
+        await expect(page.url()).toEqual(dealsUrl, 'deal url does not match');
     })
 })
 
